918a952b0ee28a334c11c5a51a28edc5f5c00eb7
Thuy Nguyen <tnntt32@gmail.com>
2012-02-26 20:12:19 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = '2d871e54d39133d093bc17a09e0e9cd0a6f74dc434b23294344f0261c9054c32bc11c445cbff3d7d185c5248a0b969501230b7453a50a8feb8c3f35475406a96'
